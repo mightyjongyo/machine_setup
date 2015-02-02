fddda8e38147fc709ac5444db8e44deb169411b2
clan <clan@eng-clan.morphodetection.com>
2015-02-02 12:48:21 -0800

    Adding vim plugins as sub-modules

--- install-vimrc.sh
+++ install-vimrc.sh
@@ -4,12 +4,3 @@ mkdir -p /home/$USER/.vim/
 ln -s `pwd`/bundle /home/$USER/.vim/
 ln -s `pwd`/autoload /home/$USER/.vim/ 
 
-cd `pwd`/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/ajh17/Spacegray.vim.git
-git clone https://github.com/bling/vim-airline
-git clone https://github.com/mileszs/ack.vim.git
-git clone https://github.com/mrtazz/DoxygenToolkit.vim
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/godlygeek/tabular.git
--- setup_subversion.sh
+++ setup_subversion.sh
@@ -1,1 +1,2 @@
-cp -r subversion /home/$USER/.subversion
+cp svn-config /home/$USER/.subversion/config
+cp svn-servers /home/$USER/.subversion/servers
