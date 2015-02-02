055baf9f73ef46fd77e985093db69cd4f4912107
clan <clan@eng-clan.morphodetection.com>
2015-02-02 13:31:00 -0800

    Removing redundant scripts

--- install-bashrc.sh
+++
@@ -1,4 +0,0 @@
-echo "Adding bashrc to local .bashrc" 
-echo "if [ -f `pwd`/bashrc ]; then" >> $HOME/.bashrc
-echo "  . `pwd`/bashrc" >> $HOME/.bashrc
-echo "fi" >> $HOME/.bashrc
--- install-vimrc.sh
+++
@@ -1,6 +0,0 @@
-echo "Adding vimrc to local .vimrc"
-echo "source `pwd`/vimrc" >> $HOME/.vimrc
-mkdir -p /home/$USER/.vim/
-ln -s `pwd`/bundle /home/$USER/.vim/
-ln -s `pwd`/autoload /home/$USER/.vim/ 
-
